5c77177790294ddae1da8bdde5983a0ad8c453ca
Rahul Madaan <rahulma@thoughtworks.com>
2020-02-15 15:39:02 +0530

    WIP

--- public/js/companies.js
+++ public/js/companies.js
@@ -1,17 +1,30 @@
-const domains = ["CMP", "PE", "MarketCap"];
+const frequentlyMoveCompanies = (src, dest) => {
+  setInterval(() => {
+    const c = src.shift();
+    if (c) dest.push(c);
+    else [src, dest] = [dest, src];
+  }, 1000);
+};
 
-const drawBuildings = (buildings, context, firstTime) => {
-  build = buildings;
+const domains = [
+  "CMP",
+  "PE",
+  "MarketCap",
+  "DivYld",
+  "QNetProfit",
+  "QSales",
+  "ROCE"
+];
+const chart = { width: 800, height: 600 };
+const margin = { top: 10, right: 10, left: 100, bottom: 150 };
 
-  const chart = { width: 800, height: 600 };
-  const margin = { top: 10, right: 10, left: 100, bottom: 150 };
+const height = chart.height - (margin.top + margin.bottom);
+const width = chart.width - (margin.left + margin.right);
 
-  const height = chart.height - (margin.top + margin.bottom);
-  const width = chart.width - (margin.left + margin.right);
-
+const generateAxis = (build, context, width, height) => {
   const y = d3
     .scaleLinear()
-    .domain([0, _.maxBy(build, context)[context]])
+    .domain([0, _.get(_.maxBy(build, context), context, 0)])
     .range([height, 0]);
 
   const x = d3
@@ -21,6 +34,31 @@ const drawBuildings = (buildings, context, firstTime) => {
     .paddingInner(0.3)
     .paddingOuter(0.3);
 
+  return { x, y };
+};
+
+const setXAxisText = (g, width, height) => {
+  g.append("text")
+    .text("Companies")
+    .attr("x", width / 2)
+    .attr("y", height + 140)
+    .attr("class", "x axislable");
+};
+
+const setYAxisText = (g, context, width, height) => {
+  g.append("text")
+    .text(`${context} (₹)`)
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height / 2)
+    .attr("y", -60)
+    .attr("class", "y axislable");
+};
+
+const drawBuildings = buildings => {
+  const context = domains[0];
+  build = buildings;
+
+  const { x, y } = generateAxis(buildings, context, width, height);
   const c = d3.scaleOrdinal(d3.schemeCategory10);
 
   svg = d3
@@ -33,18 +71,8 @@ const drawBuildings = (buildings, context, firstTime) => {
     .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  g.append("text")
-    .text("Companies")
-    .attr("x", width / 2)
-    .attr("y", height + 140)
-    .attr("class", "x axislable");
-
-  g.append("text")
-    .text(`${context} (₹)`)
-    .attr("transform", "rotate(-90)")
-    .attr("x", -height / 2)
-    .attr("y", -60)
-    .attr("class", "y axislable");
+  setXAxisText(g, width, height);
+  setYAxisText(g, context, width, height);
 
   rectangles = g.selectAll("rect").data(buildings);
 
@@ -69,19 +97,7 @@ const drawBuildings = (buildings, context, firstTime) => {
     .attr("x", -5)
     .attr("y", 10)
     .attr("text-anchor", "end");
-  update(rectangles, x, y, c, context, firstTime);
-};
 
-const formatData = ({ Name, ...numerics }) => {
-  _.forEach(numerics, (v, k) => (numerics[k] = parseInt(v)));
-  return { Name, ...numerics };
-};
-
-const update = (rectangles, x, y, c, context, bool) => {
-  if (!bool) {
-    // from second time
-    d3.select("svg").remove();
-  }
   newC = rectangles
     .enter()
     .append("rect")
@@ -92,13 +108,66 @@ const update = (rectangles, x, y, c, context, bool) => {
     .attr("fill", b => c(b.Name));
 };
 
+const updateCompanies = (companies, domain) => {
+  const svg = d3.select("#chart-area svg");
+  svg.select(".y.axislable").text(domain);
+
+  const y = d3
+    .scaleLinear()
+    .domain([0, _.maxBy(companies, domain)[domain]])
+    .range([height, 0]);
+
+  const yAxis = d3
+    .axisLeft(y)
+    .tickFormat(d => d + "₹")
+    .ticks(10);
+
+  svg.select(".y-axis").call(yAxis);
+
+  const x = d3
+    .scaleBand()
+    .range([0, width])
+    .domain(_.map(companies, "Name"))
+    .padding(0.3);
+
+  const xAxis = d3.axisBottom(x);
+  svg.select(".x-axis").call(xAxis);
+
+  svg
+    .selectAll("rect")
+    .data(companies)
+    .transition()
+    .duration(1000)
+    .ease(d3.easeLinear)
+    .attr("x", c => x(c.Name))
+    .attr("width", x.bandwidth)
+    .attr("y", c => y(c[domain]))
+    .attr("height", c => y(0) - y(c[domain]));
+
+  svg
+    .selectAll("rect")
+    .data(companies)
+    .exit()
+    .remove();
+};
+
+const formatData = ({ Name, ...numerics }) => {
+  _.forEach(numerics, (v, k) => (numerics[k] = parseInt(v)));
+  return { Name, ...numerics };
+};
+
 const main = () => {
-  buildings = d3.csv("data/companies.csv", formatData).then(t => {
+  buildings = d3.csv("data/companies.csv", formatData).then(companies => {
+    drawBuildings(companies);
     let i = 0;
-    setInterval(x => {
-      drawBuildings(t, domains[i % domains.length], i == 0);
+    setInterval(() => {
+      updateCompanies(companies, domains[i % domains.length]);
       i = i + 1;
-    }, 2000);
+    }, 1000);
+    // setInterval(() => {
+    //   domains.shift();
+    // }, 3000);
+    frequentlyMoveCompanies(companies, []);
   });
 };
 
